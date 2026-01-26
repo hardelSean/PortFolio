dcc6f810373974dd5afcde4f4cfd67457e1a714e
hardelSean <titilanderx@gmail.com>
2026-01-26 22:36:12 +0100

    fix: configuration du basePath pour le déploiement GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = "PortFolio"; // Nom exact de ton dépôt GitHub
+
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
 };
 
 export default nextConfig;
